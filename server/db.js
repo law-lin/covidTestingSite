18a63b412a54d87d032678e65be46e7ceb222229
law-lin <lawrencelin101@gmail.com>
2020-11-18 02:27:16 -0500

    Change password in db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'bongobounce123',
+  password: 'INSERT_PASSWORD_HERE',
   host: 'localhost',
   port: 5432,
   database: 'covid_testing',
