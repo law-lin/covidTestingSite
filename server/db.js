ca3cb25af682da78c1d87eb831b42c72c6a50929
law-lin <lawrencelin101@gmail.com>
2020-11-19 03:29:08 -0500

    Change db password to env var

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'INSERT_PASSWORD_HERE',
+  password: process.env.password,
   host: 'localhost',
   port: 5432,
   database: 'covid_testing',
